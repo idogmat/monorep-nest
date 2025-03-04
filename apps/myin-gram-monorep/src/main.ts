91f382480ff34f050e2a603b9bba33441d74d339
Jack R. <idogmat@gmail.com>
2025-03-04 10:59:58 +0300

    main | payments

--- apps/myin-gram-monorep/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Service B (TCP) is listening on port ${process.env.PORT || 3000}`);
-}
-bootstrap();
