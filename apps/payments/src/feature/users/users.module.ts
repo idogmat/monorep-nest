8fbfbfe6ca7e2f0f61ea9492249e568e5ce924ee
Jack R. <idogmat@gmail.com>
2025-03-04 19:52:05 +0300

    orm

--- apps/payments/src/feature/users/users.module.ts
+++ apps/payments/src/feature/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-// import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   providers: [
-    // UsersService,
-    // PrismaService
+    UsersService,
+    PrismaService
   ],
   controllers: [UsersController]
 })
--- apps/payments/test/app.e2e-spec.ts
+++ apps/payments/test/app.e2e-spec.ts
@@ -2,9 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app/app.module';
+import { PrismaClient } from '@prisma/client';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaClient;
+
+  beforeAll(async () => {
+    prisma = new PrismaClient();
+    await prisma.$connect();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
