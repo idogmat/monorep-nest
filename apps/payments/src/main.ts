253d2bc844b5b589b537e0da1c019e06a578f4f7
Jack R. <idogmat@gmail.com>
2025-03-04 23:01:52 +0300

    generate

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { useContainer } from 'class-validator';
 import { applyAppSettings } from './settings/main.settings';
 
