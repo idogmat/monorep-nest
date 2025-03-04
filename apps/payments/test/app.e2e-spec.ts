1ad17d54a37fc5e25a3490c0ec71e585ee2291c8
Jack R. <idogmat@gmail.com>
2025-03-04 17:20:23 +0300

    f-1

--- apps/payments/test/app.e2e-spec.ts
+++ apps/payments/test/app.e2e-spec.ts
@@ -17,7 +17,7 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/api/v1')
+      .get('/')
       .expect(200)
       .expect('Hello World!');
   });
