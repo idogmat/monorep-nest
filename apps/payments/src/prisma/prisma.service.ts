1e38d689f281ee81652d2685949bef6d4df28cbd
Jack R. <idogmat@gmail.com>
2025-03-04 20:45:04 +0300

    orm

--- apps/payments/src/prisma/prisma.service.ts
+++ apps/payments/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from './generated/prisma-client';
+import { PrismaClient } from './generated';
 
 @Injectable()
 export class PrismaService
