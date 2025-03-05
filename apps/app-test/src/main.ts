c319dcca92e300f2a1332faed7b068de70f1b345
Jack R. <idogmat@gmail.com>
2025-03-05 15:10:47 +0300

    second-app

--- apps/app-test/src/main.ts
+++ apps/app-test/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppTestModule } from './app-test.module';
+import 'dotenv/config'
 
 async function bootstrap() {
+  const host = process.env.TEST_HOST || '127.0.0.1';
+  const port: number = +process.env.PORT || 3001;
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppTestModule, {
     transport: Transport.TCP,
-    options: { host: '127.0.0.1', port: 3001 },
+    options: { host, port},
   });
   await app.listen();
-  console.log('Service B (TCP) is listening on port 3001');
+  console.log(`Service B (TCP) is listening on port ${port}`);
 }
 bootstrap();
--- apps/payments/src/app/app.module.ts
+++ apps/payments/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '../feature/users/users.module';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { getConfiguration } from '../settings/getConfiguration';
@@ -13,16 +13,34 @@ import { getConfiguration } from '../settings/getConfiguration';
       envFilePath: '.env',
       load: [getConfiguration]
     }),
-    ClientsModule.register([
-      {
-        name: 'TCP_SERVICE',
-        transport: Transport.TCP,
-        options: {
-          host: '127.0.0.1',
-          port: 3001,  // Порт, на который отправляется запрос в Service B
-        },
+    ClientsModule.registerAsync([
+    {
+      imports: [ConfigModule],
+      name: 'TCP_SERVICE',
+      useFactory: (configService: ConfigService) => {
+        console.log(configService.get('HOST_TCP'));
+        console.log(configService.get('CONNECT_PORT'));
+        return {
+          transport: Transport.TCP,
+          options: {
+            host: configService.get('HOST_TCP'),
+            port: configService.get('CONNECT_PORT'),  // Порт, на который отправляется запрос в Service B
+          },
+        };
       },
-    ]),
+      inject: [ConfigService],
+    },
+  ]),
+    // ClientsModule.registerAsync([
+    //   {
+    //     name: 'TCP_SERVICE',
+    //     transport: Transport.TCP,
+    //     options: {
+    //       host: '127.0.0.1',
+    //       port: 3001,  // Порт, на который отправляется запрос в Service B
+    //     },
+    //   },
+    // ]),
     UsersModule
   ],
   controllers: [AppController],
