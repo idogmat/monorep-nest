08d93b87f9db8d58520d22eaee44c33c11574ed0
Jack R. <idogmat@gmail.com>
2025-03-05 15:42:58 +0300

    second-app

--- apps/app-test/src/main.ts
+++ apps/app-test/src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppTestModule } from './app-test.module';
-import 'dotenv/config'
+// import 'dotenv/config'
 
 async function bootstrap() {
-  const host = process.env.TEST_HOST || '127.0.0.1';
-  const port: number = +process.env.PORT || 3001;
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppTestModule, {
     transport: Transport.TCP,
-    options: { host, port},
+    options: { host:'0.0.0.0', port: 3783},
   });
   await app.listen();
-  console.log(`Service B (TCP) is listening on port ${port}`);
+  console.log(`Service B (TCP) is listening on port ${3783}`);
 }
 bootstrap();
