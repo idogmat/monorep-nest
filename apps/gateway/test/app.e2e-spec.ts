8c78193c3eb17d524bd8c962ab5990a1cf23d149
drozdovalena <drozdova.el.gen@gmail.com>
2025-03-11 14:58:30 +0100

    add .env.test

--- apps/gateway/test/app.e2e-spec.ts
+++ apps/gateway/test/app.e2e-spec.ts
@@ -19,12 +19,12 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  // it('/ (GET)', () => {
-  //   return request(app.getHttpServer())
-  //     .get('/')
-  //     .expect(200)
-  //     .expect('Hello World!');
-  // });
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!');
+  });
 
   it('/ (test 1 to be 1)', () => {
     expect(1).toBe(1);
