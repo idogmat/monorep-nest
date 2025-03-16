bc68004c804f6b4cf5a19e12bf56524d57f2abc5
idogmat <idogmat@gmail.com>
2025-03-16 12:28:07 +0300

    remove migrations

---
+++ apps/gateway/prisma/migrations/20250316092758_init/migration.sql
@@ -0,0 +1,52 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "confirmationCode" TEXT,
+    "codeExpiration" TIMESTAMP(3),
+    "isConfirmed" BOOLEAN NOT NULL DEFAULT false,
+    "recoveryCode" TEXT,
+    "passwordHash" TEXT,
+    "expirationDate" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "providers" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "googleId" TEXT,
+    "githubId" TEXT,
+
+    CONSTRAINT "providers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "title" VARCHAR(255) NOT NULL,
+    "authorId" TEXT,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "providers_userId_key" ON "providers"("userId");
+
+-- AddForeignKey
+ALTER TABLE "providers" ADD CONSTRAINT "providers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
