5bcb659c9e346be7aba0022395ec9681747fdb99
idogmat <idogmat@gmail.com>
2025-03-16 16:31:31 +0300

    validate user

--- apps/gateway/src/feature/user-accounts/users/infrastructure/prisma/dto/validation.user.model.ts
+++ apps/gateway/src/feature/user-accounts/users/infrastructure/prisma/dto/validation.user.model.ts
@@ -1,12 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger"
-import { IsString } from "class-validator";
-import { ParsedQs } from "qs";
+
 
 export class ValidationUserModel {
   @ApiProperty({ type: String, nullable: true, required: false })
-  @IsString()
-  name: any
+  name: string | undefined
   @ApiProperty({ type: String, nullable: true, required: false })
-  @IsString()
-  email: any
+  email: string | undefined
 }
