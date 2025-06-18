12a6e8c44b25cff6ab7bf3a3342f920e0b06927c
drozdovalena <drozdova.el.gen@gmail.com>
2025-06-18 22:30:16 +0200

    fix error URL

--- apps/files/src/features/files/application/use-cases/upload.profile.photo.use-case.ts
+++ apps/files/src/features/files/application/use-cases/upload.profile.photo.use-case.ts
@@ -28,6 +28,7 @@ export class UploadProfilePhotoUseCase implements ICommandHandler<UploadProfileP
   async execute(command: UploadProfilePhotoCommand) {
     const { req, userId, filename } = command
 
+
     const filePath = `./uploads/${filename}`;
     const writeStream = createWriteStream(filePath, { highWaterMark: 64 * 1024 });
 
@@ -52,10 +53,11 @@ export class UploadProfilePhotoUseCase implements ICommandHandler<UploadProfileP
       // Загружаем в S3
       const folder = `profile/${userId}`;
       const uploadResult = await this.profileService.uploadImage(fileInfo, folder);
-      const photoUrl = await this.profileService.getFileUrl(uploadResult.Key);
-      console.log(photoUrl, 'photoUrl')
+      console.log('uploadResult', uploadResult)
+      // const photoUrl = await this.profileService.getFileUrl(uploadResult.Key); - здесь создается ссылка которая протухает, можно выставить макс 7 дней
+      console.log(uploadResult.Location, 'photoUrl------')
       // Отправляем сообщение в RabbitMQ
-      const message = { photoUrl, userId, timestamp: new Date() };
+      const message = { photoUrl: uploadResult.Location, userId, timestamp: new Date() };
       this.rabbitClient.emit('load_profile_photo', message);
 
       return HttpStatus.OK;
